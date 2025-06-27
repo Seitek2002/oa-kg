d1013bffc13705123e8b0fb8a59761362325bb17
Bekjan02 <bayganaliev@mail.ru>
2025-06-27 07:39:52 +0600

    feat: enhance identification process page with user status display and add check-circle icon

--- src/pages/IdentificationProcess/IdentificationProcess.tsx
+++ src/pages/IdentificationProcess/IdentificationProcess.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
 import { IonPage, IonButton } from '@ionic/react';
 
-import identificationProcessImg from '../../assets/identification-processing.svg'; // замените на нужную иллюстрацию
+import { useGetCurrentUserQuery } from '../../services/api';
+
+import identificationProcessImg from '../../assets/identification-processing.svg';
+import checkCircle from '../../assets/check-circle.svg';
 import './styles.scss';
 
 const IdentificationProcess: React.FC = () => {
+  const { data: user } = useGetCurrentUserQuery();
   return (
     <IonPage className='identification-process-page'>
       <div className='identification-process-content'>
-        <h2 className='identification-process-title'>
-          Идентификация в процессе
-        </h2>
-        <img
-          src={identificationProcessImg}
-          alt='Идентификация'
-          className='identification-process-img'
-        />
-        <div className='identification-process-subtitle'>
-          Подождите пока идет процесс
-          <br />
-          идентификации
-        </div>
+        {user?.identificationStatus === 'approved' && (
+          <>
+            <img src={checkCircle} alt='check-circle' />
+            <div className='identification-process-approved'>
+              Вы идентифицированы
+            </div>
+          </>
+        )}
+        {user?.identificationStatus === 'pending' && (
+          <>
+            <h2 className='identification-process-title'>
+              Идентификация в процессе
+            </h2>
+            <img
+              src={identificationProcessImg}
+              alt='Идентификация'
+              className='identification-process-img'
+            />
+            <div className='identification-process-subtitle'>
+              Подождите пока идет процесс
+              <br />
+              идентификации
+            </div>
+          </>
+        )}
         <IonButton
           expand='block'
           className='identification-process-btn'
